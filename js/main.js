2498fbbbb7cd3d032d0c142045e0694d2df24f8d
scgroupone <127013135+scgroupone@users.noreply.github.com>
2023-03-05 11:16:23 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+let togglebtn = document.getElementsByClassName('toggler')[0]
+let navbar = document.querySelector('.navbar')
+
+
+let links = document.getElementsByClassName('navbarlinks')[0]
+togglebtn.addEventListener('click',()=>{
+    links.classList.toggle('active')
+    navbar.classList.toggle('active')
+
+})
+window.addEventListener('DOMContentLoaded',()=>{
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET','../data/data.json',true);
+    xhr.send();
+    xhr.onload = ()=>{
+        let display = (courses)=>{
+            for(let course of courses){
+                template += ` <div class="course">
+                <ul>
+                <li><strong>Course:</strong> ${course.name}</li>
+                <li><strong>Description:</strong> ${course.description}</li>
+                <li><strong>Duration:</strong> ${course.duration}</li>
+                <li><strong>Level:</strong> ${course.level}</li>
+            </ul></div>`
+            }
+            document.querySelector('.course-display').innerHTML=template
+
+        }
+        let template ='';
+        if(xhr.status===200){
+            let data = xhr.responseText;
+            let courses = JSON.parse(data)
+            console.log(courses);
+            display(courses)
+          
+        };
+        
+    };
+});
